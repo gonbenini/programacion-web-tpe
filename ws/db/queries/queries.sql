71a54035970af789335e0a0143c87bd3d3f88d09
FranMaxx <francisco.lozanomiraglia@gmail.com>
2026-09-02 23:39:38 -0300

    tp2 - agrego consultas restantes

--- ws/db/queries/queries.sql
+++ ws/db/queries/queries.sql
@@ -3,7 +3,18 @@ INSERT INTO Usuario (nombre, mail, contrasenia)
 VALUES ($1, $2, $3)
 RETURNING id_usuario, nombre, mail, fecha_creacion;
 
+-- name: GetUserById :one
+SELECT nombre, mail
+FROM Usuario
+WHERE id_usuario = $1;
+
 -- name: ListUsers :many
-SELECT id_usuario, nombre, mail, fecha_creacion
+SELECT id_usuario, nombre, mail
 FROM Usuario
 ORDER BY id_usuario;
+
+-- name: UpdateUser :exec
+UPDATE Usuario SET nombre = $2, mail = $3, contrasenia = $4 WHERE id_usuario = $1;
+
+-- name: DeleteUser :exec
+DELETE FROM Usuario WHERE id_usuario = $1;
